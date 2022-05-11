34ea5082690eb69581bd97b6688560f6d22e7537
Михаил <Ibraam_Gaunt@mail.ru>
2022-05-11 17:34:32 +0300

    ryad3

--- THERD.py
+++ THERD.py
@@ -1,10 +1,6 @@
 a= int(input())
 b= int(input())
-if a>b:
-    for i in range(b, a + 1):
-        a = a - 1
-        print(a + 1)
-else:
-    for i in range(a, b + 1):
+for i in range(a, b + 1,2):
         a = a + 1
+
         print(a - 1)
