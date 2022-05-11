6823c96e0ef1f1c9d44cb3a9a4f2c4c2ab1f9d2b
Михаил <Ibraam_Gaunt@mail.ru>
2022-05-11 20:01:18 +0300

    lost card

--- THERD.py
+++ THERD.py
@@ -1,5 +1,11 @@
 b=int(input())
-for i in range(0,b):
-        for j in range (0,i+1):
-                print (j+1, sep='', end=' ')
-        print()
+a=int(0)
+d=int(0)
+c=int(0)
+
+for i in range(0,b+1):
+       a=a+i
+for i in range(0, b-1):
+        c=int(input())
+        d = d + c
+print (a-d)
