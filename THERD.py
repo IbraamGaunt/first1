8119d9c0286a8da7df7c74b5a3cb60dcf9f3ba62
Михаил <Ibraam_Gaunt@mail.ru>
2022-05-11 19:31:29 +0300

    lesenka

--- THERD.py
+++ THERD.py
@@ -1,7 +1,5 @@
 b=int(input())
-e=0
 for i in range(0,b):
-        a=int(input())
-        if a==0:
-                e=e+1
-print(e)
+        for j in range (0,i+1):
+                print (j+1, sep='', end=' ')
+        print()
