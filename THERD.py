d5b3c1776f23060e6c7f12bab429bc17efe84e97
Михаил <Ibraam_Gaunt@mail.ru>
2022-05-12 12:22:52 +0300

    timer

--- THERD.py
+++ THERD.py
@@ -1,11 +1,8 @@
-b=int(input())
-a=int(0)
-d=int(0)
-c=int(0)
-
-for i in range(0,b+1):
-       a=a+i
-for i in range(0, b-1):
-        c=int(input())
-        d = d + c
-print (a-d)
+import time
+import sys
+for remaining in range(10, 0, -1):
+    sys.stdout.write("\r")
+    sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+    sys.stdout.flush()
+    time.sleep(1)
+sys.stdout.write("\rComplete!            \n")
